c8d11fa91ac069e01a3a39915c0c047f599767ac
Lucasvirtual <o_lukinhas@hotmail.com>
2023-02-07 19:54:53 -0300

    Atualizando diretorio

--- energy.py
+++ energy.py
@@ -1,59 +1,61 @@
+from importlib.resources import path
 import winreg as rg
 from tkinter import *
 from tkinter import ttk
 import winreg
+import time
+import schedule
 
-def modoEnergia():
-    rg.CreateKeyEx(rg.HKEY_LOCAL_MACHINE,r"SOFTWARE\Microsoft\Windows\CurrentVersion\explorer\ControlPanel\NameSpace\{6bff0ef8-7e5f-4252-a2fc-061dbb1a8571}")
-
-
-    
+#-------------------------------------------------------------------#
 
 janela = Tk()
 janela.title("Energy Manager")
 text_orientacao = Label(janela, text="Energy Manager")
 janela.geometry("320x320")
 
-listEnergia=["Economia","Alto desempenho"]
+#-------------------------------------------------------------------#
 
-lb_energia=Label(text="Modo de energia atual")
-lb_energia.pack()
+def modo_Atual():
+    
+    ativar = rg.OpenKey(rg.HKEY_LOCAL_MACHINE,r"SOFTWARE\Microsoft\Windows\CurrentVersion\explorer\ControlPanel\NameSpace\{a1841308-3541-4fab-bc81-f71556f20b4a}")
+    print  ("Ola")
+    
+    
 
-cb_energia=ttk.Combobox(values=listEnergia)
-cb_energia.set("")
-cb_energia.pack()
-
-path = winreg.HKEY_CURRENT_USER
-
-software = winreg.OpenKeyEx(path, r"SOFTWARE\\")
-new_key = winreg.CreateKey(software, "NeuralNine")
+    
 
 
-#--------------------------------------------#
-listTempo=["1min","3min","5min","10min","15min","30min"]
 
-lb_energia=Label(text="Tempo para mudar")
-lb_energia.pack()
+#listEnergia=["Economia","Alto desempenho"]
 
-cb_energia=ttk.Combobox(values=listTempo)
-cb_energia.set("1min")
-cb_energia.pack()
+#lb_energia=Label(text="Modo de energia atual")
+#lb_energia.pack()
 
-
+#cb_energia=ttk.Combobox(values=listEnergia)
+#cb_energia.set("")
+#cb_energia.pack()#
 
 #--------------------------------------------#
-lb_energia=Label(text="Modo de Energia que irá mudar")
-lb_energia.pack()
+#listTempo=["1min","3min","5min","10min","15min","30min"]
 
-cb_energia=ttk.Combobox(values=listEnergia)
-cb_energia.set("Economia")
-cb_energia.pack()
+#lb_energia=Label(text="Tempo para mudar")
+#lb_energia.pack()
+
+#cb_energia=ttk.Combobox(values=listTempo)
+#cb_energia.set("1min")
+#cb_energia.pack()
+
+#--------------------------------------------#
+#lb_energia=Label(text="Modo de Energia que irá mudar")
+#lb_energia.pack()
+
+#cb_energia=ttk.Combobox(values=listEnergia)
+#cb_energia.set("Economia")
+#cb_energia.pack()
 
 #-----------------------------------------#
 
-btnIniciar = Button(text = "Iniciar",command=modoEnergia) .place(x=130,y=140)
-
-
-
+btnIniciar = Button(text = "Iniciar",  command = modo_Atual)
+btnIniciar.place(x=130,y=140)
 
 janela.mainloop()
