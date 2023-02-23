12928a465dfa23c22041c76c8c41f7850b1cffe1
Lucasvirtual <o_lukinhas@hotmail.com>
2023-02-23 10:25:00 -0300

    Adicionando para note

--- energymanager2_notebook.py
+++ energymanager2_notebook.py
@@ -9,14 +9,14 @@ janela.geometry("350x250")
 def economia():
     # Define o plano de energia para Economia
     
-    subprocess.run(["powercfg", "/setactive", "a1841308-3541-4fab-bc81-f71556f20b4a"])   
+    subprocess.run(["powercfg", "/setactive", "a6230e07-0388-4a44-a49f-0389adfe16e6"])   
     labelAtual["text"] = "Modo de energia agora é: Economia"
 
 
 def altodesempenho():
     # Define o plano de energia para Alto Desempenho
     
-    subprocess.run(["powercfg", "/setactive", "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"])
+    subprocess.run(["powercfg", "/setactive", "fed2c136-42f5-4a5b-a02a-6e9d961aa6cd"])
     labelAtual["text"] = "Modo de energia agora é: Alto Desempenho"
 
 def check_idle_time():
@@ -29,14 +29,14 @@ def iniciar():
 def check_idle():
     idle_time = check_idle_time()
 
-    if idle_time >= 5 * 1000: # Se o tempo de inatividade for igual ou superior a 5 segundos
+    if idle_time >= 600 * 1000: # Se o tempo de inatividade for igual ou superior a 5 segundos
         labelAtual.config(text="Modo Economia ativado")
-        subprocess.run(["powercfg", "/setactive", "a1841308-3541-4fab-bc81-f71556f20b4a"])                  
+        subprocess.run(["powercfg", "/setactive", "a6230e07-0388-4a44-a49f-0389adfe16e6"])                  
         #labelInfo["text"] = subprocess.run(["powercfg", "/getactivescheme"])
         
     else:        
         labelAtual.config(text="Modo Alto Desempenho ativado")
-        subprocess.run(["powercfg", "/setactive", "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"])
+        subprocess.run(["powercfg", "/setactive", "fed2c136-42f5-4a5b-a02a-6e9d961aa6cd"])
         #labelInfo["text"] = subprocess.run(["powercfg", "/getactivescheme"])
     
     if not stop:
